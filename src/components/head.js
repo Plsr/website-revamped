aa74ff6f3ecbc630ef53d0a903f2362e8f5fbb92
Plsr <christianpoplawski@gmail.com>
2022-08-14 13:05:07 +0200

    Add webmention headers

--- src/components/head.js
+++ src/components/head.js
@@ -2,6 +2,10 @@ import * as React from 'react'
 
 export const Head = () => {
   return (
-    <link rel="me" href="https://github.com/Plsr" />
+    <>
+      <link rel="me" href="https://github.com/Plsr" />
+      <link rel="webmention" href="https://webmention.io/chris-new-website.netlify.app/webmention" />
+      <link rel="pingback" href="https://webmention.io/chris-new-website.netlify.app/xmlrpc" />
+    </>
   )
 }
